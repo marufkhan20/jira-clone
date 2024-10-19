f81314ac6790743ac285b1114fe8e05447f071d5
Rashedul Islam Maruf <mdm326040@gmail.com>
2024-10-19 13:28:11 +0600

    Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div>
-      <Button size="lg">Click Me</Button>
-      <p></p>
+    <div className="flex gap-4 p-10">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">teritary</Button>
     </div>
   );
 };
