5603e603e2f74f0174ce558b7b0f427290a1ce51
Rashedul Islam Maruf <mdm326040@gmail.com>
2024-10-26 22:33:22 +0600

    Build dashboard layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,16 +1,11 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 const Home = async () => {
   const user = await getCurrent();
 
   if (!user) redirect("/sign-in");
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div>This is a home page.</div>;
 };
 
 export default Home;
